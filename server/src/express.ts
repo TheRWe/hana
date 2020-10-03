b6cb8699ec71397791771ede707d6afe53403c5b
Roman Wehmhöner <r.wehmhoner@gmail.com>
2020-10-03 19:55:21 +0200

    fix: react dev port changed

--- server/src/express.ts
+++ server/src/express.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
+import { existsSync } from "fs";
 import * as open from "open";
 import * as path from "path";
+
+const dirUi = path.join(__dirname, "../ui/");
 
 export const startExpressServer = async () => {
   const app = express();
@@ -8,17 +11,23 @@ export const startExpressServer = async () => {
 
   app.use(express.json());
 
-  app.use("/", express.static(path.join(__dirname, "../ui/")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"../ui/index.html"));
-  });
-
   app.listen(port, () => {
     // tslint:disable-next-line: no-console
     console.info(`listening on: http://localhost:${port}`);
   });
 
-  open("http://localhost:3000/");
+  // in dev mode we have no build of ui
+  if (existsSync(dirUi)) {
+    app.use("/", express.static(dirUi));
 
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "../ui/index.html"));
+    });
+
+    open("http://localhost:3000/");
+  } else {
+    app.get("/", (req, res) => {
+      res.redirect("http://localhost:3001/");
+    });
+  }
 };
