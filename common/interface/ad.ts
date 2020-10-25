84919c75918b2f33e995e4bdaa04c8f6c8535db2
benak66 <adam.bilovice@seznam.cz>
2020-10-25 17:46:46 +0100

    Added Ad category and StockExchange category

--- common/interface/ad.ts
+++ common/interface/ad.ts
@@ -1,7 +1,13 @@
 import { TCommonData } from "./common";
 
+export enum TAdCategory {
+  Supply = "Supply",
+  Demand = "Demand",
+}
+
 export type TAd = TCommonData & {
   isValId: boolean,
+  category: TAdCategory,
   type: string,
   salary: number,
 };
--- common/interface/stockExchange.ts
+++ common/interface/stockExchange.ts
@@ -1,7 +1,14 @@
 import { TCommonData } from "./common";
 
+export enum TStockCategory {
+  Buy = "Buy",
+  Sell = "Sell",
+  Exchange = "Exchange",
+}
+
 export type TStockExchange = TCommonData & {
   isValId: boolean,
+  category: TStockCategory,
   type: string,
   cost: number,
 };
