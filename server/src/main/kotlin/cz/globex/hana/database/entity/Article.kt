0861afc4f8fab2b16289e3c430e4829729600c1a
kamil <kamil.kotlar@icloud.com>
2020-11-09 02:16:06 +0100

    fix: improved example entity encapsulation

--- server/src/main/kotlin/cz/globex/hana/database/entity/Article.kt
+++ server/src/main/kotlin/cz/globex/hana/database/entity/Article.kt
@@ -4,7 +4,7 @@ import javax.persistence.*
 
 @Suppress("ProtectedInFinal")
 @Entity
-data class Article(
+data class Article protected constructor(
 	@Column(nullable = false)
 	var title: String,
 
@@ -16,8 +16,15 @@ data class Article(
 	@Column(updatable = false)
 	val id: Int = 0
 
-	@Suppress("RemoveEmptyClassBody")
-	companion object {}
+	companion object {
+		fun newInstance(title: String, text: String): Article {
+			val trimmedTitle = title.trim()
+			val trimmedText = text.trim()
+			if (trimmedTitle.isEmpty() || trimmedText.isEmpty()) throw IllegalArgumentException()
+
+			return Article(title = trimmedTitle, text = trimmedText)
+		}
+	}
 
 	protected constructor() : this("", "")
 }
