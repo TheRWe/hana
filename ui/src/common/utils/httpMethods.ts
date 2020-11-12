af9b384340b433cb7681955109777ba4babba8e5
Sciator <39964450+Sciator@users.noreply.github.com>
2020-11-12 14:58:17 +0100

    style: moved utils to sciator-core npm pack

--- ui/src/common/utils/httpMethods.ts
+++
@@ -1,2 +0,0 @@
-
-export type THttpMethod = "GET" | "POST" | "PUT" | "DELETE";
--- ui/src/common/utils/promise.ts
+++
@@ -1,7 +0,0 @@
-
-/** create promise that resolves after given ms' */
-export const sleep = async (ms: number) => {
-  return new Promise((r) => {
-    setTimeout(() => r(), ms);
-  });
-};
