a12cc1949b0685cecfde19ee494d36911331e296
Bulhy <stojmenov.martin@email.cz>
2020-12-05 20:03:03 +0100

    feat(ui): added details pages + component

---
+++ ui/src/pages/EventDetails.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Details } from "../components/Details";
+
+type TEventDetailsProps = {
+
+};
+
+export const EventDetails: React.FC<TEventDetailsProps> = () => {
+
+  return <>
+    <section>
+      <Details
+        imagePath="../images/no_image.png"
+        heading="Nadpis"
+        text="nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat "
+        entryFee="Zdarma"
+        telephoneNumber="789456123"
+      ></Details>
+    </section>
+  </>;
+};
---
+++ ui/src/pages/JobAdDetails.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Details } from "../components/Details";
+
+type TJobAdDetailsProps = {
+
+};
+
+export const JobAdDetails: React.FC<TJobAdDetailsProps> = () => {
+
+  return <>
+    <section>
+      <Details
+        imagePath="../images/no_image.png"
+        heading="Nadpis"
+        text="nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat nevim co napsat "
+        pay="130 Kč/h"
+        telephoneNumber="789456123"
+      ></Details>
+    </section>
+  </>;
+};
